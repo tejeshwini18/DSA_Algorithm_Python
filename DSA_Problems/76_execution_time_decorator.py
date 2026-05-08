2f81f3d242c6ac70fef7aaf66c0904c157a873ac
Tejeshwini Sahu <76488126+tejeshwini18@users.noreply.github.com>
2026-05-08 20:15:14 +0530

    Add files via upload

---
+++ DSA_Problems/76_execution_time_decorator.py
@@ -0,0 +1,15 @@
+import time
+def execution_time_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time: {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+@execution_time_decorator
+def my_function():
+    time.sleep(2)
+
+my_function()
